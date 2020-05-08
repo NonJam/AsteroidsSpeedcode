8ae82f7fd6720d7eb735fcbe3a866a1263c780ad
Eva Wong <suddenlyeva@gmail.com>
2020-05-07 19:42:00 -0700

    2D Physics

--- asteroid/src/main.rs
+++ asteroid/src/main.rs
@@ -1,15 +1,59 @@
-//use std::error::Error;
+
 use tetra::graphics::{self, Color, Texture};
 use tetra::{Context, ContextBuilder, State};
 
-//type WithResult<T> = Result<T, Box<dyn Error>>;
 
 struct GameState {
     asteroid_tex: Texture,
+    asteroids: Vec<Physics2D>
+}
+
+struct Physics2D {
+  x: f64,
+  y: f64,
+  dx: f64,
+  dy: f64,
+  speed: Option<f64>,
+  angle: Option<f64>,
+  accel: Option<f64>,
+}
+
+impl Physics2D {
+  fn apply_physics(&mut self) {
+
+    if let (Some(speed), Some(angle)) = (self.speed, self.angle) {
+
+      if let Some(accel) = self.accel {
+        self.speed = Some(speed + accel)
+      }
+
+      self.dx = angle.to_radians().sin() * speed;
+      self.dy = angle.to_radians().cos() * speed;
+
+    }
+
+    self.x += self.dx;
+    self.y += self.dy;
+
+  }
+  fn default() -> Physics2D {
+    Physics2D {
+      x: 0f64,
+      y: 0f64,
+      dx: 0f64,
+      dy: 0f64,
+      speed: None,
+      angle: None,
+      accel: None
+    }
+  } 
 }
 
 impl State for GameState {
     fn update(&mut self, ctx: &mut Context) -> tetra::Result {
+      for asteroid in self.asteroids.iter_mut() {
+        asteroid.apply_physics();
+      }
         Ok(())
     }
 
@@ -23,7 +67,8 @@ impl State for GameState {
 impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
         Ok(GameState {
-           asteroid_tex: Texture::new(ctx, "asteroid.png")?,
+          asteroids: vec![],
+          asteroid_tex: Texture::new(ctx, "asteroid.png")?,
         })
     }
 
