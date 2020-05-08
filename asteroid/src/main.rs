b5dcd4986fb90f17e87b27ef7bdbd4c4ae05b181
Ellen <supbscripter@gmail.com>
2020-05-08 03:36:34 +0100

    Add draw asteroid method

--- asteroid/src/main.rs
+++ asteroid/src/main.rs
@@ -1,11 +1,12 @@
 //use std::error::Error;
-use tetra::graphics::{self, Color};
+use tetra::graphics::{self, Color, Texture};
 use tetra::{Context, ContextBuilder, State};
 
 //type WithResult<T> = Result<T, Box<dyn Error>>;
 
-
-struct GameState;
+struct GameState {
+    asteroid_tex: Texture,
+}
 
 impl State for GameState {
     fn update(&mut self, ctx: &mut Context) -> tetra::Result {
@@ -19,10 +20,33 @@ impl State for GameState {
     }
 }
 
+impl GameState {
+    fn new(ctx: &mut Context) -> tetra::Result<GameState> {
+        Ok(GameState {
+           asteroid_tex: Texture::new(ctx, "asteroid.png")?,
+        })
+    }
+
+    /// # This draws an asteroid at position 500,500 that has a radius of 100
+    /// 
+    /// self.draw_asteroid(ctx, 500f64, 500f64, 100f64);
+    fn draw_asteroid(&self, ctx: &mut Context, x: f64, y: f64, r: f64) {
+        use tetra::math::Vec2;
+        use tetra::graphics::DrawParams;
+        let scale = r / 1024f64;
+        let params = DrawParams::new()
+            .position(Vec2::new(x as f32, y as f32))
+            .scale(Vec2::new(scale as f32, scale as f32))
+            .origin(Vec2::new(512f32, 512f32));
+    
+        graphics::draw(ctx, &self.asteroid_tex, params);
+    }
+}
+
 fn main() -> tetra::Result {
   ContextBuilder::new("Hello, world!", 1280, 720)
       .build()?
-      .run(|_| Ok(GameState))
+      .run(GameState::new)
 }
 
 fn overlaps(x1: f64, y1: f64, r1: f64, x2: f64, y2: f64, r2: f64) -> bool {
