0feb91c77682953bfdd7a7144180b3dd95d13fa3
Ellen <supbscripter@gmail.com>
2020-05-08 03:53:04 +0100

    formatting cahnges

--- asteroid/src/main.rs
+++ asteroid/src/main.rs
@@ -1,65 +1,77 @@
-
 use tetra::graphics::{self, Color, Texture};
 use tetra::{Context, ContextBuilder, State};
 
+struct Physics2D {
+    x: f64,
+    y: f64,
+    r: f64,
+    dx: f64,
+    dy: f64,
+    speed: Option<f64>,
+    angle: Option<f64>,
+    accel: Option<f64>,
+}
+
+impl Physics2D {
+    fn new(x: f64, y: f64, radius: f64) -> Self {
+        let mut blah = Physics2D::default();
+        blah.x = x;
+        blah.y = y;
+        blah.r = radius;
+        blah
+    }
+
+    fn apply_physics(&mut self) {
+        if let (Some(speed), Some(angle)) = (self.speed, self.angle) {
+            if let Some(accel) = self.accel {
+                self.speed = Some(speed + accel)
+            }
+
+            self.dx = angle.to_radians().sin() * speed;
+            self.dy = angle.to_radians().cos() * speed;
+        }
+
+        self.x += self.dx;
+        self.y += self.dy;
+    }
+}
+
+impl Default for Physics2D {
+    fn default() -> Self {
+        Physics2D {
+            x: 0f64,
+            y: 0f64,
+            r: 0f64,
+            dx: 0f64,
+            dy: 0f64,
+            speed: None,
+            angle: None,
+            accel: None
+        }
+    }
+}
 
 struct GameState {
     asteroid_tex: Texture,
     asteroids: Vec<Physics2D>
 }
 
-struct Physics2D {
-  x: f64,
-  y: f64,
-  dx: f64,
-  dy: f64,
-  speed: Option<f64>,
-  angle: Option<f64>,
-  accel: Option<f64>,
-}
-
-impl Physics2D {
-  fn apply_physics(&mut self) {
-
-    if let (Some(speed), Some(angle)) = (self.speed, self.angle) {
-
-      if let Some(accel) = self.accel {
-        self.speed = Some(speed + accel)
-      }
-
-      self.dx = angle.to_radians().sin() * speed;
-      self.dy = angle.to_radians().cos() * speed;
-
-    }
-
-    self.x += self.dx;
-    self.y += self.dy;
-
-  }
-  fn default() -> Physics2D {
-    Physics2D {
-      x: 0f64,
-      y: 0f64,
-      dx: 0f64,
-      dy: 0f64,
-      speed: None,
-      angle: None,
-      accel: None
-    }
-  } 
-}
-
 impl State for GameState {
     fn update(&mut self, ctx: &mut Context) -> tetra::Result {
-      for asteroid in self.asteroids.iter_mut() {
-        asteroid.apply_physics();
-      }
+        for asteroid in self.asteroids.iter_mut() {
+            asteroid.apply_physics();
+        }
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
         // Cornflower blue, as is tradition
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+        for ast in self.asteroids.iter() {
+            self.draw_asteroid(ctx, ast.x, ast.y, ast.r);
+        }
+
         Ok(())
     }
 }
@@ -67,8 +79,8 @@ impl State for GameState {
 impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
         Ok(GameState {
-          asteroids: vec![],
-          asteroid_tex: Texture::new(ctx, "asteroid.png")?,
+            asteroids: vec![],
+            asteroid_tex: Texture::new(ctx, "asteroid.png")?,
         })
     }
 
