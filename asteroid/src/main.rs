23c7331e3effe6109b7fb9cd21a5a3cbe44441dd
Eva Wong <suddenlyeva@gmail.com>
2020-05-07 18:29:35 -0700

    make window

--- asteroid/src/main.rs
+++ asteroid/src/main.rs
@@ -1,8 +1,22 @@
-use std::error::Error;
+//use std::error::Error;
+use tetra::graphics::{self, Color};
+use tetra::{Context, ContextBuilder, State};
 
-type WithResult<T> = Result<T, Box<dyn Error>>;
+//type WithResult<T> = Result<T, Box<dyn Error>>;
 
-fn main() -> WithResult<()> {
-    println!("Hello, world!");
-    Ok(())
+
+struct GameState;
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+        // Cornflower blue, as is tradition
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+        Ok(())
+    }
+}
+
+fn main() -> tetra::Result {
+  ContextBuilder::new("Hello, world!", 1280, 720)
+      .build()?
+      .run(|_| Ok(GameState))
 }
